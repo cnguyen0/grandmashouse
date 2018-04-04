44be080c67974aa34a60f177d69814ed8d3ac4b9
maxwhalezhou <nerdtothemax@gmail.com>
2018-04-04 16:43:21 -0700

    Updated to use material-ui@next

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from '../logo.svg';
+import './App.css';
+import ListingCard from './ListingCard.js'
+
+class App extends Component {
+  render() {
+    return (
+      <ListingCard />
+    );
+  }
+}
+
+export default App;
