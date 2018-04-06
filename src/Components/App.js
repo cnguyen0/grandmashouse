7e8be9371e10f84dbd142dae2afc63cea5e3544d
maxwhalezhou <nerdtothemax@gmail.com>
2018-04-06 00:53:28 -0700

    Added Google maps as MyMapComponent

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import logo from '../logo.svg';
 import './App.css';
 import ListingCard from './ListingCard.js'
 import MenuAppBar from './MenuAppBar.js'
+// import GridList from './GridList.js'
+import MyMapComponent from './MyMapComponent.js'
+
+
 
 class App extends Component {
-  getRecipe = () => {
-    // api call to the database
-    // return object
-  }
+  // getRecipe = () => {
+  //   // api call to the database
+  //   // return object
+  // }
+
 
   render() {
     // let chefRecipe = getRecipe();
@@ -20,8 +24,11 @@ class App extends Component {
     //       )
     //     })
     //   }
+
+
       <div>
         <MenuAppBar />
+        <MyMapComponent />
         <ListingCard />
       </div>
     );
