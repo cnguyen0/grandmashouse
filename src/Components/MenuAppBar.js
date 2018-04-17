4609b0bc20c023760871c931808d4c451f8d1511
Cindy Nguyen <cnguyen0@uw.edu>
2018-04-16 22:20:04 -0700

    search bar

--- src/Components/MenuAppBar.js
+++ src/Components/MenuAppBar.js
@@ -10,6 +10,7 @@ import MenuIcon from 'material-ui-icons/Menu';
 import AccountCircle from 'material-ui-icons/AccountCircle';
 import Menu, { MenuItem } from 'material-ui/Menu';
 import Button from 'material-ui/Button';
+import Search from './Search';
 
 const SignIn = props => <Link to="/signin" {...props} />
 const Reservations = props => <Link to="/reservations" {...props} />
@@ -20,13 +21,10 @@ const styles = {
   root: {
     flexGrow: 1,
   },
-  flex: {
-    flex: 1,
-  },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
-  },
+  }
 };
 
 class MenuAppBar extends React.Component {
@@ -59,25 +57,26 @@ class MenuAppBar extends React.Component {
             <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
               <MenuIcon />
             </IconButton>
-            <Typography variant="title" color="inherit" className={classes.flex}>
+            <Typography variant="title" color="inherit">
               <a id='HomeBtn' href='/'>Grandma&#39;s House</a>
             </Typography>
-              <Button variant="raised" color="primary" className={classes.button} component={Favorites}>
-                Favorites
+            <Search className={classes.search}/>
+            <Button variant="raised" color="primary" className={classes.button} component={Favorites}>
+              Favorites
+            </Button>
+            <Button variant="raised" color="primary" className={classes.button} component={Reservations}>
+              Reservations
+            </Button>
+            <Button variant="raised" color="primary" className={classes.button} component={Messages}>
+              Messages
+            </Button>
+            <Button variant="raised" color="secondary" className={classes.button}>
+              Become a Chef
+            </Button>
+            { !this.state.auth && <Button variant="raised" color="secondary" className={classes.button} component={SignIn}>
+                Sign In
               </Button>
-              <Button variant="raised" color="primary" className={classes.button} component={Reservations}>
-                Reservations
-              </Button>
-              <Button variant="raised" color="primary" className={classes.button} component={Messages}>
-                Messages
-              </Button>
-              <Button variant="raised" color="secondary" className={classes.button}>
-                Become a Chef
-              </Button>
-              { !this.state.auth && <Button variant="raised" color="secondary" className={classes.button} component={SignIn}>
-                  Sign In
-                </Button>
-              }
+            }
             {auth && (
               <div>
                 <IconButton
